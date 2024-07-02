d9da6a63d38ed9bfc6a690c2ff911a85df96a5bd
tharushi <tharushig8@gmail.com>
2024-07-02 15:59:41 +1000

    fetch working

--- Charitix/src/frontend/index.jsx
+++ Charitix/src/frontend/index.jsx
@@ -1,40 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
+import ForgeReconciler, { Text, Button, Image } from '@forge/react';
 import { invoke } from '@forge/bridge';
+import { fetch } from '@forge/api';
 
 const App = () => {
   const [data, setData] = useState(null);
 
+  // useEffect(() => {
+  //   invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+  // }, []);
+
   useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+    invoke('getEvents').then(setData);
   }, []);
 
-  options = {
-      method:"GET",
-      headers: {
-          'Accept': 'application/json',
-          "X-API-KEY": "ef14e306e672f390217db34781064f8b7d6020b88276abace03e8f3bd62bc1a18953f1f89140777bb87861a2fa1e452608721be5c18c99b6cdce6e8f062ffe65366801f5256e1b8572c3cc77f1aa51a077d6c67c0259f9cfc7982434ff9887b624db6cf038cbbe2e60216d21a5ebee"
-      }
-  }
 
+  console.log(data)
 
-  async function getEvents () {
-    const response = await fetch("https://api.humanitix.com/v1/events?page=1", {
-      method:"GET",
-      headers: {
-          'Accept': 'application/json',
-          "X-API-KEY": "ef14e306e672f390217db34781064f8b7d6020b88276abace03e8f3bd62bc1a18953f1f89140777bb87861a2fa1e452608721be5c18c99b6cdce6e8f062ffe65366801f5256e1b8572c3cc77f1aa51a077d6c67c0259f9cfc7982434ff9887b624db6cf038cbbe2e60216d21a5ebee"
-      }
-    })
-    const data = await response.json()
-    console.log(data)
-  }
 
   return (
     <>
-      <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
-      <button onClick={getEvents()}></button>
+      <Text>Hello world!</Text> 
+      <Button>press me</Button>
+      <Text>{data 
+      ? <>
+      {}
+      {data.name} 
+      {data.description}
+      
+      </>
+      : 'Loading...'}</Text>
     </>
   );
 };
--- Charitix/src/resolvers/index.js
+++ Charitix/src/resolvers/index.js
@@ -1,4 +1,5 @@
 import Resolver from '@forge/resolver';
+import { fetch } from '@forge/api';
 
 const resolver = new Resolver();
 
@@ -7,5 +8,19 @@ resolver.define('getText', (req) => {
 
   return 'Hello, world!';
 });
+
+resolver.define('getEvents', async () => {
+  const response = await fetch("https://api.humanitix.com/v1/events?page=1", {
+    method:"GET",
+    headers: {
+        'Accept': 'application/json',
+        "X-API-KEY": process.env.HUMANITIX_KEY
+    }
+  })
+  console.log(response)
+  const data = await response.json()
+  console.log(data)
+  return data
+})
 
 export const handler = resolver.getDefinitions();
